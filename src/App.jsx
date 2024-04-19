359fc572494989cee750510c4a7638e9adfa1c46
HenriPham280100 <phamk883@gmail.com>
2024-04-19 21:08:33 +0700

    Updated: add StrictMode for App

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,32 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 import { About, Contact, Home, Projects } from "./pages";
 import NavBar from "./component/NavBar";
 import Footer from "./component/Footer";
+import { StrictMode } from "react";
 
 const App = () => {
   return (
     <main className="bg-slate-300/20 h-full">
-      <HashRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/*"
-            element={
-              <>
-                <Routes>
-                  <Route path="/about" element={<About />} />
-                  <Route path="/projects" element={<Projects />} />
-                  <Route path="/contact" element={<Contact />} />
-                </Routes>
-                <Footer />
-              </>
-            }
-          />
-        </Routes>
-      </HashRouter>
+      <StrictMode>
+        <HashRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/*"
+              element={
+                <>
+                  <Routes>
+                    <Route path="/about" element={<About />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/contact" element={<Contact />} />
+                  </Routes>
+                  <Footer />
+                </>
+              }
+            />
+          </Routes>
+        </HashRouter>
+      </StrictMode>
     </main>
   );
 };
