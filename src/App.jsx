da7f7f4c002888a94045bc703a4d96a813a6018c
HenriPham280100 <phamk883@gmail.com>
2024-04-19 09:59:10 +0700

    update router

--- src/App.jsx
+++ src/App.jsx
@@ -11,27 +11,25 @@ import Footer from "./component/Footer";
 const App = () => {
   return (
     <main className="bg-slate-300/20 h-full">
-      <Router>
+      <HashRouter>
         <NavBar />
-        <HashRouter>
-          <Routes>
-            <Route path="/3D-Portfolio/" element={<Home />} />
-            <Route
-              path="/*"
-              element={
-                <>
-                  <Routes>
-                    <Route path="/3D-Portfolio/about" element={<About />} />
-                    <Route path="/3D-Portfolio/projects" element={<Projects />} />
-                    <Route path="/3D-Portfolio/contact" element={<Contact />} />
-                  </Routes>
-                  <Footer />
-                </>
-              }
-            />
-          </Routes>
-        </HashRouter>
-      </Router>
+        <Routes>
+          <Route path="/3D-Portfolio/" element={<Home />} />
+          <Route
+            path="/*"
+            element={
+              <>
+                <Routes>
+                  <Route path="/3D-Portfolio/about" element={<About />} />
+                  <Route path="/3D-Portfolio/projects" element={<Projects />} />
+                  <Route path="/3D-Portfolio/contact" element={<Contact />} />
+                </Routes>
+                <Footer />
+              </>
+            }
+          />
+        </Routes>
+      </HashRouter>
     </main>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import * as ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const router = createBrowserRouter([
+  { path: "/3D-Portfolio/", element: <App /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(<RouterProvider router={router}/>);
